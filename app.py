c61dca09e74578bb587a3d957554c629210d9e39
leen19-meet <leen19@meet.mit.edu>
2018-08-14 09:19:13 +0300

    update merge

--- app.py
+++ app.py
@@ -15,26 +15,17 @@ def home():
 def register_student():
     return render_template('register_student.html')
 
-<<<<<<< HEAD
-@app.route('/register_employer')
-def register_employer():
-    return render_template('register_employeer.html')
-=======
 @app.route('/register_employeer')
 def register_employeer():
     return render_template('register_employee.html')
->>>>>>> 73b206629f85d061402079082a5eb7a3ea008760
 
 @app.route('/login')
 def login():
     return render_template('login.html')
 
-<<<<<<< HEAD
-=======
 @app.route('/logedin')
 def logedin():
     return render_template('home2.html')
->>>>>>> 73b206629f85d061402079082a5eb7a3ea008760
 
 # Running the Flask app
 if __name__ == "__main__":
